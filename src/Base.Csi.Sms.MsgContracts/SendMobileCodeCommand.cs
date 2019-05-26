4314962e57d3d463678ec71a9d6e5968709c752e
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2019-05-26 23:31:16 +0800

    added Base.Csi.Sms.MsgContracts

---
+++ src/Base.Csi.Sms.MsgContracts/SendMobileCodeCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base.Csi.Sms.MsgContracts
+{
+    /// <summary>
+    /// 发送手机验证码
+    /// </summary>
+    public interface SendMobileCodeCommand
+    {
+        string[] PhoneNumbers { get; set; }
+        string Code { get; set; }
+    }
+}
